91b8b67449e57c84d4b56fa1783d86bd48a12ef0
Craig Otis <craigotis@gmail.com>
2016-04-02 09:52:38 -0400

    Updates the window to be frameless, adds a window title, and updates screenshots.

--- main.js
+++ main.js
@@ -7,53 +7,54 @@ var dialog = require('dialog');
 var mainWindow = null;
 
 var template = [{
-  label: 'File',
-  submenu: [{
-    label: 'Open',
-    accelerator: 'Command+O',
-    click: function() {
-      dialog.showOpenDialog(mainWindow, {
-        properties: ['openFile', 'multiSelections', 'createDirectory']
-      }, onFileOpen);
-    }
-  }, {
-    label: 'Quit',
-    accelerator: 'Command+Q',
-    click: function() {
-      app.quit();
-    }
-  }]
+    label: 'File',
+    submenu: [{
+        label: 'Open',
+        accelerator: 'Command+O',
+        click: function() {
+            dialog.showOpenDialog(mainWindow, {
+                properties: ['openFile', 'multiSelections', 'createDirectory']
+            }, onFileOpen);
+        }
+    }, {
+        label: 'Quit',
+        accelerator: 'Command+Q',
+        click: function() {
+            app.quit();
+        }
+    }]
 }];
 
 app.on('window-all-closed', function onWindowAllClosed() {
-  app.quit();
+    app.quit();
 });
 
 app.on('ready', function onReady() {
-  mainWindow = new BrowserWindow({
-    width: 1024,
-    height: 600
-  });
+    mainWindow = new BrowserWindow({
+        width: 1024,
+        height: 600,
+        titleBarStyle: 'hidden'
+    });
 
-  var menu = Menu.buildFromTemplate(template);
-  Menu.setApplicationMenu(menu);
+    var menu = Menu.buildFromTemplate(template);
+    Menu.setApplicationMenu(menu);
 
-  delete mainWindow.module;
+    delete mainWindow.module;
 
-  if (process.env.ELECTRON_ENV === 'development') {
-    mainWindow.openDevTools();
-    mainWindow.loadURL('http://localhost:5000');
-  } else {
-    mainWindow.loadURL('file://' + __dirname + '/dist/index.html');
-  }
+    if (process.env.ELECTRON_ENV === 'development') {
+        mainWindow.openDevTools();
+        mainWindow.loadURL('http://localhost:5000');
+    } else {
+        mainWindow.loadURL('file://' + __dirname + '/dist/index.html');
+    }
 
-  mainWindow.on('closed', function onClosed() {
-    mainWindow = null;
-  });
+    mainWindow.on('closed', function onClosed() {
+        mainWindow = null;
+    });
 });
 
 function onFileOpen(files) {
-  if (files && files.length) {
-    mainWindow.webContents.send('filesOpened', JSON.stringify(files));
-  }
+    if (files && files.length) {
+        mainWindow.webContents.send('filesOpened', JSON.stringify(files));
+    }
 }
